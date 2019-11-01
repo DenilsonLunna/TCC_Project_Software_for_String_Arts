f99c76404d225d4bfac263c5c5a10b1041dde950
DenilsonLunna <denilsonsousa7@outlook.com>
2019-11-01 16:46:54 -0300

    Codigo funcionando

--- Projeto TCC/main.py
+++ Projeto TCC/main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from tkinter import filedialog
 from math import sin, cos, radians
 from random import randint
 import copy 
@@ -16,7 +17,7 @@ def putFilterBlackWhite(imagem, limiar):
 
 
 def printImage(image, time):
-    cv2.imshow("soccer", image)
+    cv2.imshow("imagem", image)
     key = cv2.waitKey(time)
     if key == 27:
         cv2.destroyAllWindows()
@@ -42,7 +43,10 @@ def nailsCreate(image, nailsQuantity):
         x = int(center[0] + ((center[0] - 1) * sin(-(radians(angle * i)))))
         y = int(center[1] + ((center[0] - 1) * cos(-(radians(angle * i)))))
         # image.itemset((y, x), 0)
-        image[y:y+3, x: x+3] = 0
+        if(image[y,x] == 0):
+            image[y:y+5, x: x+5] = 255 #adicionando ponto na imagem no tamanho 3x3 pixels
+        else:
+            image[y:y+5, x: x+5] = 0 #adicionando ponto na imagem no tamanho 3x3 pixels
         nail_positions.append([x, y])
     return image
 
@@ -75,6 +79,8 @@ def algorithmWeaver():
     actual_point = nail_positions[0]
     segments = [0]
     lines = 0
+    cont1 = 0
+    cont2 = 0
     while True:
         bigger = [0, nail_positions[0], nail_positions[0]]
         for i in range(0, nailsQuantity):
@@ -90,33 +96,75 @@ def algorithmWeaver():
         cv2.line(canvas, (line[1][0], line[1][1]), (line[0][0], line[0][1]), (0, 0, 0), 1)
         image_output_1 = cv2.resize(img, (700, 700))
         image_output_2 = cv2.resize(canvas, (700, 700))
+        
+        #essa parte comentada é para criar as imagens no decorrer do processo, ele vai salvar imagens com multiplos de 20 e multiplos de 250
+        #usei para gerar uma imagem não apague pois posso precisar de novo
+        '''if((lines < 200) and (lines % 20) == 0):
+            cv2.imwrite("img_{}_.png".format(cont1),image_output_1)
+            cv2.imwrite("imgString_{}_{}lines_.png".format(cont2,lines),image_output_2)
+            cont1 = cont1 +1
+            cont2 = cont2 +1
+        if ((lines % 250) == 0):
+            cv2.imwrite("img_{}_.png".format(cont1),image_output_1)
+            cv2.imwrite("imgString_{}_{}lines_.png".format(cont2,lines),image_output_2)
+            cont1 = cont1 +1
+            cont2 = cont2 +1'''
+            
         cv2.imshow('output_1', image_output_1)
         cv2.imshow('output_2', image_output_2)
         k = cv2.waitKey(1)
         if(k == 27):
             break
         lines += 1
-        if bigger[0] == 0 or lines == 1800:
+        if bigger[0] == 0 or lines == 2500:
             break
         
-        
+def cutImage(imagem):
+    y = imagem.shape[0]
+    x = imagem.shape[1]
+    yM = int(y/2)
+    xM = int(x/2)
+    smaller = 0
+    if(yM < xM):
+        smaller = yM
+    else:
+        smaller = xM
+   
+    if(smaller == yM):
+        initialPoint = yM - smaller
+    else:
+        initialPoint = xM - smaller
+    
+    tam = smaller*2
+    
+    crop = imagem[initialPoint:tam,initialPoint:tam]
+    crop = cv2.resize(crop,(1000,1000))
+    return crop
+    
+    
+    
+    
 # ===================================================== MAIN ============================================
-imagemName = "Homem.png"
-imagem = cv2.imread(imagemName, 1)
+imagemPath = filedialog.askopenfilename()
+imagem = cv2.imread(imagemPath, 1)
+
 img = editImage(imagem)
-printImage(img,3000)
+
+
+img = cutImage(img)
 nail_positions = []
 nailsQuantity = 210
 canvas = createCanvas(img)
 canvas = nailsCreate(canvas,nailsQuantity)
 img = nailsCreate(img, nailsQuantity)
-
+cv2.imwrite("etapaNails.png",img)
 
 
 algorithmWeaver()
 
-    
-cv2.imwrite("StringArt_{}".format(imagemName),canvas)
+fileSeparated = imagemPath.split("/")
+nameFile = fileSeparated[len(fileSeparated) - 1]
+#cv2.imwrite("StringArt_{}".format(nameFile),canvas)
     
     
             

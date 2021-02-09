64a3d33b5a34ccb5ee464efd7362192e061ab979
Denilson <denilsonsousa7@outlook.com>
2021-02-09 09:24:12 -0300

    Bug imagem distorcida resolvido

--- Projeto TCC/telas/app.py
+++ Projeto TCC/telas/app.py
@@ -3,28 +3,20 @@ from tkinter import filedialog
 import cv2
 
 
+def getBiggerSize(img):
+    if(img.shape[0] < img.shape[1]):
+        return img.shape[0]
+    else:
+        return img.shape[1]
+    
 
-#function to crop image
-#Parameters: image to crop, contour, and the image number
-def crop(image, contours, num):
-    idNum = 0
-    #cycles through all the contours to crop all
-    for c in contours:
-    #creates an approximate rectangle around contour
-        x,y,w,h = cv2.boundingRect(image)
-        # Only crop decently large rectangles
-        if w>50 and h>50:
-            idNum+=1
-            #pulls crop out of the image based on dimensions
-            new_img=image[y:y+h,x:x+w]
-            #writes the new file in the Crops folder
-#returns a number incremented up for the next file name
-    return num+1
+
     
 def cutImage(img, initial):
     w = img.shape[0]
     h = img.shape[1]
-    
+    if(w == h):
+        return img
     if(h > w):
         img = img[0:initial+w,initial:initial+w]
     else:
@@ -60,8 +52,8 @@ class TelaPython():
         layout = [
             [sg.Button('Load image', key="loadImage")],
             
-            [sg.Text("Limiar Value: ", key="textLimiar", visible=False), sg.Text("", key="textSizeImage", visible=True), 
-             sg.Slider(range=(0,255),default_value = 80, orientation='h', key="sliderLimiar",change_submits=True, visible=False)],
+            [sg.Text("Limiar Value: ", key="textLimiar", visible=False), sg.Text("W = 10000 H = 10000", key="textSizeImage", visible=False,font = 'arial')], 
+            [sg.Slider(range=(0,255),default_value = 80, orientation='h', key="sliderLimiar",change_submits=True, visible=False)],
             [sg.Text("Adjust Image: ", visible=True)], [sg.Slider(range=(0,255),default_value =0, orientation='h', key="sliderCutWidth",change_submits=True, visible=False)],  
             [sg.Image(filename='', key='image')],   
             [sg.Button('OK')]
@@ -78,23 +70,29 @@ class TelaPython():
             if(self.events== "loadImage"):
                 imagemPath = filedialog.askopenfilename()
                 imagem = cv2.imread(imagemPath, 1)
-                #logica para corte da imagem
+               
                 
                 
                 self.janela["textLimiar"].update(visible=True)
                 self.janela["sliderLimiar"].update(visible=True)
                 self.janela["sliderCutWidth"].update(visible=True)
-                
+                self.janela["textSizeImage"].update(visible=True)
                 
                
-                text = "Image Size = Width = {} Height = {}".format(imagem.shape[0],imagem.shape[1])
+                text = "W = {} H = {}".format(imagem.shape[1],imagem.shape[0])
                 self.janela["textSizeImage"].update(value = text)
                 
                 imagemCrop = cutImage(imagem,0)
                 imagemBG = transformImageInBlackAndGray(imagem)
-                imagem = cutImage(imagem,500)
+                
                 imagem = putFilterBlackWhite(imagemBG, self.values["sliderLimiar"])
-                imagem = cv2.resize(imagem, (500,500))  
+                imagem = cutImage(imagem,getBiggerSize(imagem))
+                imagem = cv2.resize(imagem, (500,500))
+                
+                #Essa parte é pra resolver um bug
+                imagem = putFilterBlackWhite(imagemBG, self.values["sliderLimiar"])
+                imagem = cutImage(imagem,int(self.values["sliderCutWidth"]+1)) 
+                imagem = cv2.resize(imagem, (500,500)) 
                  
                 imgbytes = cv2.imencode('.png', imagemBG)[1].tobytes()  # ditto 
                 self.janela["image"].update(data=imgbytes)
@@ -135,7 +133,7 @@ def qtdBlack(image):
                 qtd += 1
     return qtd
 
-#tela = TelaPython()
-#tela.iniciar()
+tela = TelaPython()
+tela.iniciar()
 
 print("done")
